c7b8988c7dcadbf3ed9d5ca4f1a0a7c9f303f10b
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-02-15 11:43:34 +0600

    Add files via upload

---
+++ 1011.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    double r,v;
+    cin>>r;
+
+    v = (4.0/3.0)*3.14159*pow(r,3);
+
+    printf("VOLUME = %.3lf\n",v);
+
+
+
+    return 0;
+}
